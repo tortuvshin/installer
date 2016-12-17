6066b271947cbf64bd0d8950aedc373a107a14d2
Tortuvshin Byambaa <tortuvshin@users.noreply.github.com>
2016-12-17 22:40:58 +0800

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -20,6 +20,8 @@ echo "Git амжилттай суулаа..."
 sleep 1
 echo "java суулгах гэж байна." 
 sleep 3
+
+sudo apt-get install default-jdk
 sudo apt-get purge openjdk*
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
