c08621a84e1ffbf5805e3453a919633b2ee79a23
tortuvshin <toroo.byamba@gmail.com>
2017-01-24 15:43:37 +0800

    lib modules

---
+++ lib/git.sh
@@ -0,0 +1,11 @@
+function install_git {
+	sudo add-apt-repository ppa:git-core/ppa
+	sudo apt-get update
+	sudo apt-get install git
+
+	sudo apt-get update
+	sudo apt-get install build-essential libssl-dev libcurl4-gnutls-dev libexpat1-dev gettext unzip
+
+	git config --global user.name "Tortuvshin Byambaa"
+	git config --global user.email "toroo.byamba@gmail.com"
+}
