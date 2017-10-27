c547caa9b468fa532da24f9b2ab2aea350493b3f
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2017-10-27 18:49:49 +0800

    java 9 install

--- installer.sh
+++ installer.sh
@@ -28,8 +28,8 @@ sudo apt-get install default-jdk
 sudo apt-get purge openjdk*
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
-sudo apt-get install oracle-java8-installer oracle-java8-set-default
-echo "Installed..."
+sudo apt-get install oracle-java9-installer
+echo "Java Installed..."
 sleep 3 
 
 echo "Chromium install"
